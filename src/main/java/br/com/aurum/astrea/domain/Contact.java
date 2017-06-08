cfbd98219fb723507f05a15738f7d929ebe2ca4a
diegomfloripa <diegomelodev@gmail.com>
2017-06-08 03:25:32 -0300

    Add global cache na entidade Contact

--- src/main/java/br/com/aurum/astrea/domain/Contact.java
+++ src/main/java/br/com/aurum/astrea/domain/Contact.java
@@ -2,11 +2,13 @@ package br.com.aurum.astrea.domain;
 
 import java.util.List;
 
+import com.googlecode.objectify.annotation.Cache;
 import com.googlecode.objectify.annotation.Entity;
 import com.googlecode.objectify.annotation.Id;
 import com.googlecode.objectify.annotation.Index;
 
 @Entity
+@Cache
 public class Contact {
 
 	@Id
